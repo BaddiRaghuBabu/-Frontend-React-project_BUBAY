0583e9e7bb5dd938268c559b03aa817f6eabc4a8
BaddiRaghuBabu <baddiraghuram6@gmail.com>
2025-03-20 14:57:57 +0530

    path changed from asset/funnyimage

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NotFound.css"; // CSS for animations
+import funny404 from "../../assets/funny404.png";
+
 
 const NotFound = () => {
   return (
     <div className="not-found-container">
-      <img src="./funny404.png" alt="Funny 404" className="funny-img" />
+<img src={funny404} alt="Funny 404" className="funny-img" />      
       <h1 className="bounce">404</h1>
       <h2>Oops! Looks like you're lost.</h2>
       <p>Even our astronaut couldn't find this page! 😵</p>
